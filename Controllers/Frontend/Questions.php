d90f4ba8225a0b55c973ff40c6c02bfc77447f40
max <max@swk-web.com>
2017-06-23 14:36:07 +0200

    Rename controller for Shopware naming conventions

---
+++ Controllers/Frontend/Questions.php
@@ -0,0 +1,14 @@
+<?php
+class Shopware_Controllers_Frontend_Questions extends Enlight_Controller_Action
+{
+    public function preDispatch()
+    {
+        $pluginPath = $this->container->getParameter('lorem_questions.plugin_dir');
+
+        $this->get('template')->addTemplateDir($pluginPath . '/Resources/views/');
+    }
+
+    public function indexAction()
+    {
+    }
+}
